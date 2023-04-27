b1d9ca6aab852f948dbaffab7540408509a6d7a0
harimrim <kigkfla@naver.com>
2023-04-27 15:41:23 +0900

    ProductCardCell autolayout 설정
    - xib를 사용한 ProductCardCell autolayout 설정

---
+++ Onban/Onban/Screen/Home/View/Cell/ProductCardCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCardCell.swift
+//  Onban
+//
+//  Created by 김하림 on 2023/04/27.
+//
+
+import UIKit
+
+class ProductCardCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
